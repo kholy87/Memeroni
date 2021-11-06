165174721e64da787c54ab2ff986fa3c40db1a1e
jeffrey.wood-dev <jwood@credence-llc>
2021-11-05 23:31:53 -0400

    jj command begun

--- shared/player.js
+++ shared/player.js
@@ -24,7 +24,7 @@ module.exports = async function playSoundFile(interaction){
         inlineVolume: true,
         metadata:{
             title: 'title',
-        }
+        },
     });
     async function start(){
         player.play(resource);
